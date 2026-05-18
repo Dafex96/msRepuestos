9b9ca9a4cdd58e29ce29427982986db32e68896b
Dafex96 <davicolo717@gmail.com>
2026-05-18 17:57:51 -0400

    listoListo

--- src/main/java/cl/duoc/msRepuestos/controller/RepuestoController.java
+++ src/main/java/cl/duoc/msRepuestos/controller/RepuestoController.java
@@ -8,13 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import cl.duoc.msRepuestos.dto.RepuestoDTO;
 import cl.duoc.msRepuestos.model.Repuesto;
 import cl.duoc.msRepuestos.service.RepuestoService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
